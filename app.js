496b66dcb180bddb34ec73e72558b63359afac2e
Alex <galexwade25@gmail.com>
2021-09-08 13:32:18 -0500

    added static blogs

--- app.js
+++ app.js
@@ -6,20 +6,27 @@ app.set('view engine', 'ejs');
 
 // Home page
 app.get('/', (req, res) => {
-    res.render('index');
-});
-// About page
-app.get('/about', (req, res) => {
-    res.render('about');
+    const blogs = [
+        {title: "Drake drops CLB", snippet: "lorem ipsum dolor sit amet consectectur"},
+        {title: "Kanye drops Donda", snippet: "lorem ipsum dolor sit amet consectectur"},
+        {title: "Kendrick drops a single", snippet: "lorem ipsum dolor sit amet consectectur"}
+    ];
+    res.render('index', { title: "Home", blogs: blogs });
 });
 
+// About page
+app.get('/about', (req, res) => {
+    res.render('about', { title: "About" });
+});
+
+// Create a blog page
 app.get('/blogs/create', (req, res) => {
-    res.render()
+    res.render('create', { title: "Create a new blog" })
 })
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: "404" });
 });
 
 const port = process.env.port || 3000;
