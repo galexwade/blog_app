f4a0bb773159fdfbe4bfb656cbec74ab1fdc7555
Alex <galexwade25@gmail.com>
2021-09-08 13:14:31 -0500

    all ejs pages created

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const app = express();
 // Register view engine
 app.set('view engine', 'ejs');
 
+// Home page
 app.get('/', (req, res) => {
     res.render('index');
+});
+// About page
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render()
+})
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404');
 });
 
 const port = process.env.port || 3000;
